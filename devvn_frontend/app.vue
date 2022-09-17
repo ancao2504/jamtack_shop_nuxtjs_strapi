c1ec2982507d10734adb392b81491c7decc22411
andevct96 <andevct96@gmail.com>
2022-09-18 02:04:16 +0700

    init source dev

---
+++ devvn_frontend/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="main-taiwinds">
+      <div class="container">
+          <Product />
+          <News />
+      </div>
+    </div>
+    
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Product from '@/components/Products/Home/Product-Lists.vue'
+  export default {
+    components: {
+        Product
+    }
+  }
+</script>
